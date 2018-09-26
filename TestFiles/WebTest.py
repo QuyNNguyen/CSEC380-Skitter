9ee69daf130bf074c6547d9dd6e92a22392c5167
Ferrari <jxf9385@rit.edu>
2018-09-26 14:21:02 -0400

    fixed python script to run locust

--- TestFiles/WebTest.py
+++ TestFiles/WebTest.py
@@ -1,6 +1,6 @@
 import shlex, subprocess
 
-args = shlex.split("locust --host://localhost")
+args = shlex.split("locust -f TestFiles/locustfile.py --host=http://localhost")
 
 p1 = subprocess.Popen(args)
 
